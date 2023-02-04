98e400b30cb156b9c0774d55ba7572a07a4603bc
Ilya8788 <122901504+Ilya8788@users.noreply.github.com>
2023-02-04 17:55:32 +0200

    Add files via upload

---
+++ Project/burger.js
@@ -0,0 +1,17 @@
+function burger_init() {
+  // הכפתור של הבורגר צריך את האיי די הזה
+  // console.log("burger")
+  let burger_btn = document.querySelector("#burger_btn");
+
+  // התפריט שייעלם ויחזור צריך
+  // את האיי די הזה
+  let id_nav_show_hide = document.querySelector("#id_nav_show_hide");
+
+  burger_btn.addEventListener("click", function () {
+    id_nav_show_hide.style.display =
+      id_nav_show_hide.style.display != "flex" ? "flex" : "none";
+    console.log("Click");
+  });
+}
+
+burger_init();
